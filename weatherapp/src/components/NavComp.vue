db63c8b95a5208e3289ccd3cfa50d47dcbc30a0f
Andrei Danila <115635392+bigsosa337@users.noreply.github.com>
2023-04-10 19:41:23 +0300

    Creating the navigation bar

---
+++ weatherapp/src/components/NavComp.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="container add-city">
+    <nav>
+        <span>Add City</span>
+        <div class="right">
+            <i class="far fa-edit"></i>
+            <i class="far fa-sync"></i>
+            <i class="far fa-plus"></i>
+        </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "NavComponent",
+}
+</script>
+
+<style lang="scss" scoped>
+.add-city {
+        background-color: #313640;
+    }
+header {
+    
+    nav {
+        display: flex;
+        color: #ffff;
+        padding: 30px 0;
+        justify-content: space-between;
+    }
+}
+
+</style>
