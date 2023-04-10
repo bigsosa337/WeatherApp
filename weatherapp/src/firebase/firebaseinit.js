c56eaeb81b787150f728c3d1261020217d117ec1
Andrei Danila <115635392+bigsosa337@users.noreply.github.com>
2023-04-10 17:29:25 +0300

    Initialize db

---
+++ weatherapp/src/firebase/firebaseinit.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCCD0eY3cZeBobfMobuc2NL1k1FuNMqErU",
+    authDomain: "weatherapp-9c54f.firebaseapp.com",
+    projectId: "weatherapp-9c54f",
+    storageBucket: "weatherapp-9c54f.appspot.com",
+    messagingSenderId: "1009550147468",
+    appId: "1:1009550147468:web:a21a954d58b417961be0a9",
+    measurementId: "G-H93JGR4S7S"
+  };
+
+  //Initialize firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  export default firebaseApp.firestore();
